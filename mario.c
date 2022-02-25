9be61f52b31f2affd9b88cf461ef2b44831f114e
Alaa Elsanteel <78312680+alaaelsanteel@users.noreply.github.com>
2022-02-26 00:40:03 +0200

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int hight;
+    do
+    {
+        hight = get_int("Hight: ");
+    }
+    while (hight > 8 || hight < 1);
+    for (int i = 1; i <= hight; i++)
+    {
+        for (int j = 0; j < hight - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
